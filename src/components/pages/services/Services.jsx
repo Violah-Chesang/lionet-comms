6cb25aa30401dc1d7789fe5f10cc695188e76b12
Violah Chesang <chesangv21@gmail.com>
2024-01-06 12:33:54 +0300

    Added images

---
+++ src/components/pages/services/Services.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './services.css';
+
+export default function Services() {
+  return (
+    <div className='services' id='services'>
+        <h1>Services Page</h1>
+    </div>
+  )
+}
