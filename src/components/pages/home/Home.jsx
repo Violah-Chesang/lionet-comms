f9fff52d0880ae7daa6e08ad8d8aa0b542f58764
Violah Chesang <chesangv21@gmail.com>
2024-01-08 16:57:08 +0300

    Had chaanged to functional based component but the reverted

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Nav from '../../nav/Nav';
 import Main from '../../pages/main/Main';
 import About from '../about/About';
 import Services from '../../pages/services/Services';
 import Contact from '../contact/Contact';
 import { Route, Routes } from 'react-router-dom';
-import Fibre from '../services-single-page/fibre/Fibre';
 
-export default function Home() {
-
+export default class Home extends Component {
+  render() {
     return (
       <div className='home'>
         <Nav />
@@ -17,10 +16,8 @@ export default function Home() {
           <Route path='/about' element={<About />}></Route>
           <Route path='/services' element={<Services />}></Route>
           <Route path='/contact' element={<Contact />}></Route>
-          <Route path='/services/fibre' element={<Fibre />} ></Route>
         </Routes>
       </div>
     )
-  
+  }
 }
-
