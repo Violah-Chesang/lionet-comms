82acfba1d35a4f1d6b571a9d408337f78cf338bd
Violah Chesang <chesangv21@gmail.com>
2024-01-08 13:44:43 +0300

    Added fibre route and changed the page from class to functional component

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Nav from '../../nav/Nav';
 import Main from '../../pages/main/Main';
 import About from '../about/About';
 import Services from '../../pages/services/Services';
 import Contact from '../contact/Contact';
 import { Route, Routes } from 'react-router-dom';
+import Fibre from '../services-single-page/fibre/Fibre';
 
-export default class Home extends Component {
-  render() {
+export default function Home() {
+
     return (
       <div className='home'>
         <Nav />
@@ -16,8 +17,10 @@ export default class Home extends Component {
           <Route path='/about' element={<About />}></Route>
           <Route path='/services' element={<Services />}></Route>
           <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/services/fibre' element={<Fibre />} ></Route>
         </Routes>
       </div>
     )
-  }
+  
 }
+
