453ec513c3e6259d83af4cbbc2afee9b5d475f30
Violah Chesang <chesangv21@gmail.com>
2024-01-15 12:06:57 +0300

    Changed the z-index to a higher value

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -1,36 +1,54 @@
-import React from 'react';
-import './about.css';
-import about from '../../../../src/images/fibre.jpg';
-import mission from '../../../images/Installation.png';
-import vision from '../../../images/lionet-fibre.png';
-import values from '../../../images/lionet-support.png';
+import React from "react";
+import "./about.css";
+import about from "../../../../src/images/cabling.jpg";
+import mission from "../../../images/Installation.png";
+import vision from "../../../images/lionet-fibre.png";
+import values from "../../../images/lionet-support.png";
 
 export default function About() {
   return (
-    <div id='about' className='about'>
-      <div className='about-cover'>
-        <div className='history'>
-          <h1 className='about-title'>Who are we?</h1>
-          <img src={about} alt='fibre specialist' className='about-image' />
-          <p className='about-history'>
-          Lionet Communications Ltd Was Incorporated In The Year 2022 Having Operated For 5 Years As A Sole Proprietor Business. The Incorporation Brought On Board New Talents To Carry On With The Vision Of Becoming A Leader In Providing Business Solutions To Our Customers.
-          </p>
+    <div id="about" className="about">
+        <div className="history">
+            <div className="about-history">
+              <h1 className="about-title">Who are we?</h1>
+              <p>
+                Lionet Communications Ltd was incorporated in the year 2022
+                having operated for 5 years as a sole proprietor business. The
+                incorporation brought on board new talents to carry on with the
+                vision of becoming a leader in providing business solutions to
+                our customers.
+              </p>
+            </div>
+
+            <div className="about-image">
+              <img src={about} alt="fibre specialist" />
+            </div>
         </div>
-        <div className='about-row'>
-          <div className='about-col'>
-            <img src={mission} alt='' className='about-col-img'/>            
-            <h2 className='about-col-title'>Mission</h2>
-            <p className='about-col-details'>We Are Dedicated At Being Your Ideal Partner, By Providing Reliable And Competitive Services So As To Deliver The Best Solutions.</p>
+        <span className="about-more">More about us...</span>
+        
+        <div className="about-row">          
+          <div className="about-col">
+            <img src={mission} alt="" className="about-col-img" />
+            <h2 className="about-col-title">Mission</h2>
+            <p className="about-col-details">
+              We are dedicated at being your ideal partner, by providing
+              reliable and competitive services so as to deliver the best
+              solutions.
+            </p>
           </div>
-          <div className='about-col'>
-            <img src={vision} alt='' className='about-col-img'/>
-            <h2 className='about-col-title'>Vision</h2>
-            <p className='about-col-details'>To Continue Enhancing Customer Experience By Striving To Make Positive Difference In Your Business Through Unmatched And Superior Customer Experience.</p>
+          <div className="about-col">
+            <img src={vision} alt="" className="about-col-img" />
+            <h2 className="about-col-title">Vision</h2>
+            <p className="about-col-details">
+              To continue enhancing customer experience by striving to make
+              positive difference in your business through unmatched and
+              superior customer experience.
+            </p>
           </div>
-          <div className='about-col'>
-            <img src={values} alt='' className='about-col-img'/>
-            <h2 className='about-col-title'>Core Values</h2>
-            <p className='about-col-details'>
+          <div className="about-col">
+            <img src={values} alt="" className="about-col-img" />
+            <h2 className="about-col-title">Core Values</h2>
+            <p className="about-col-details">
               <ul>
                 <li>- Reliable Services</li>
                 <li>- Team Work</li>
@@ -40,7 +58,6 @@ export default function About() {
             </p>
           </div>
         </div>
-      </div>
     </div>
-  )
+  );
 }
